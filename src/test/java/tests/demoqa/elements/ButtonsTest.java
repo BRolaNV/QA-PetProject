609b8bf02f4cd61d0e6100e8242f8181419e91f6
Rolan <rolan.working@gmail.com>
2026-04-04 22:38:30 +0300

    Accordion test complete

--- src/test/java/tests/demoqa/elements/ButtonsTest.java
+++ src/test/java/tests/demoqa/elements/ButtonsTest.java
@@ -1,6 +1,5 @@
 package tests.demoqa.elements;
 
-import com.codeborne.selenide.ClickOptions;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
