273bed3ea784c6cad2fecef4d9e46a93149fdae1
Rolan <rolan.working@gmail.com>
2026-04-22 23:21:16 +0300

    feat: add Jenkinsfile and remote/gridUrl properties for CI

---
+++ Jenkinsfile
@@ -0,0 +1,34 @@
+pipeline {
+    agent any
+
+    tools {
+        jdk 'jdk-17'
+        maven 'maven-3.9'
+    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                checkout scm
+            }
+        }
+
+        stage('Build') {
+            steps {
+                sh 'mvn clean compile -DskipTests'
+            }
+        }
+
+        stage('Test') {
+            steps {
+                sh 'mvn test -Dremote=true -DgridUrl=http://selenium-hub:4444/wd/hub'
+            }
+        }
+    }
+
+    post {
+        always {
+            echo 'Pipeline finished'
+        }
+    }
+}
--- src/test/java/tests/demoqa/tests/BaseUITest.java
+++ src/test/java/tests/demoqa/tests/BaseUITest.java
@@ -1,21 +1,35 @@
 package tests.demoqa.tests;
-
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.AllureAttachmentsExtension;
 import io.qameta.allure.selenide.AllureSelenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 @ExtendWith(AllureAttachmentsExtension.class)
 public abstract class BaseUITest {
     @BeforeAll
     static void setUp() {
         Configuration.browser = "chrome";
+
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.timeout = 10000;
-        Configuration.pageLoadStrategy = "normal";
         Configuration.browserSize = "1920x1080";
+
+        boolean isRemote = Boolean.parseBoolean(System.getProperty("remote", "false"));
+
+        if (isRemote) {
+            String gridUrl = System.getProperty("gridUrl", "http://localhost:4444/wd/hub");
+            Configuration.remote = gridUrl;
+
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--headless=new");
+
+            Configuration.browserCapabilities = options;
+        }
 
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
                 .screenshots(true)
